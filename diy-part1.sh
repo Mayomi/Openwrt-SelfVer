46ec8c30fffe9b8ab54754c380d12268f0f4645e
Mayomi Tian <mayomi@baka.wang>
2022-03-20 01:04:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/vernesong/OpenClash.git
-cp -R OpenClash/luci-app-openclash openwrt/package
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
